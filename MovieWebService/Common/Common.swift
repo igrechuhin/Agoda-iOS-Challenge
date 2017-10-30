a549af7f18d5d56c760d1fc4170287cca36ee194
Ilya Grechuhin <i.grechuhin@gmail.com>
2017-10-30 16:28:24 +0300

    Added common class to string function.

---
+++ MovieWebService/Common/Common.swift
@@ -0,0 +1,12 @@
+//
+//  Common.swift
+//  MovieWebService
+//
+//  Created by Ilya Grechuhin on 30.10.2017.
+//  Copyright © 2017 TestCompany. All rights reserved.
+//
+
+func toString(_ cls: AnyClass) -> String {
+  return String(describing: cls)
+}
+
