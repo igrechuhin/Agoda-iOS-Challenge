deb77e75602c9772df59d21dd959ef176719adec
Ilya Grechuhin <i.grechuhin@gmail.com>
2017-10-30 16:57:25 +0300

    Exposed base viper module to swift.

--- MovieWebService/Common/Movie-Bridging-Header.h
+++ MovieWebService/Common/Movie-Bridging-Header.h
@@ -2,5 +2,11 @@
 
 #import "MWSActor.h"
 #import "MWSDirector.h"
+#import "MWSDisplayDataProtocol.h"
 #import "MWSFilm.h"
+#import "MWSInteractor.h"
+#import "MWSModule.h"
+#import "MWSPresenter.h"
+#import "MWSRouter.h"
+#import "MWSView.h"
 #import "TappableLabel.h"
