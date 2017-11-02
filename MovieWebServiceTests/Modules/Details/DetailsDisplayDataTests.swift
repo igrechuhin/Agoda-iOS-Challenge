d83caf86c6b81cdab1b59226b8b5e56afd3234a1
Ilya Grechuhin <i.grechuhin@gmail.com>
2017-11-02 18:13:46 +0300

    Added details display data tests.

---
+++ MovieWebServiceTests/Modules/Details/DetailsDisplayDataTests.swift
@@ -0,0 +1,24 @@
+//
+//  DetailsDisplayDataTests.swift
+//  MovieWebServiceTests
+//
+//  Created by Ilya Grechuhin on 02.11.2017.
+//  Copyright © 2017 TestCompany. All rights reserved.
+//
+
+@testable import MovieWebService
+
+import XCTest
+
+class DetailsDisplayDataTests: XCTestCase {
+  func testData() {
+    let data = DetailsDisplayData()
+
+    XCTAssertEqual(data.backgroundColor, UIColor.white)
+    XCTAssertEqual(data.directorNameTitle, "Director Name")
+    XCTAssertEqual(data.actorNameTitle, "Actor Name")
+    XCTAssertEqual(data.actorScreenNameTitle, "Actor Screen Name")
+    XCTAssertEqual(data.showMoreActionTitle, "Tap here to show more")
+    XCTAssertEqual(data.showLessActionTitle, "Tap here to show less")
+  }
+}
