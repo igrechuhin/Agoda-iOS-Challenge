080e1a0b8d2160ad98ac8e906722ba210650123f
Ilya Grechuhin <i.grechuhin@gmail.com>
2017-11-02 16:13:46 +0300

    Removed unused UITableView+Cells extension code.

--- MovieWebService/Extensions/UITableView+Cells.swift
+++ MovieWebService/Extensions/UITableView+Cells.swift
@@ -11,10 +11,6 @@ extension UITableView {
     register(UINib(cellClass), forCellReuseIdentifier: toString(cellClass))
   }
 
-  @objc func dequeueReusableCell(withCellClass cellClass: AnyClass) -> UITableViewCell? {
-    return dequeueReusableCell(withIdentifier: toString(cellClass))
-  }
-
   @objc func dequeueReusableCell(withCellClass cellClass: AnyClass, indexPath: IndexPath) -> UITableViewCell {
     return dequeueReusableCell(withIdentifier: toString(cellClass), for: indexPath)
   }
